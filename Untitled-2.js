a08a3ee9de5de552dc980c51a69089e77878c7f1
ScuffedPACE <167155871+ScuffedPACE@users.noreply.github.com>
2024-05-17 23:36:18 -0400

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,8 @@
+<script>
+    document.querySelector('form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Thank you for your message. We will get back to you shortly.');
+        // Here you can add the code to actually handle the form submission
+        // For example, send the form data to a server or an email service
+    });
+</script>
